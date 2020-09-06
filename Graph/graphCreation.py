58dc20a3b53afbb3b80907e25c4b5fb83a6bb6dc
SebastianProdaso <s.heger@prodaso.de>
2020-09-06 17:44:38 +0200

    Adds regular node and edge creation

--- Graph/graphCreation.py
+++ Graph/graphCreation.py
@@ -1,3 +1,10 @@
+from pathlib import Path
+import math
+import uuid
+
+from PIL import Image
+
+
 def database_reset(db):
     def delete_table(table_name):
         cur = db.cursor()
@@ -42,6 +49,7 @@ class Graph:
 
         if reinit:
             database_reset(db)
+            self.generate_graph(db)
         else:
             cur = db.cursor()
             for item in cur.execute('SELECT * FROM nodes'):
@@ -58,3 +66,77 @@ class Graph:
                     'distance': item[3]
                 }
             db.commit()
+
+    def generate_graph(self, db, accuracy=5):
+        def generate_nodes():
+            cur = db.cursor()
+            path = Path().parent / 'Input'
+            filename = 'MAP.png'
+            image = Image.open(path / filename, 'r')
+            width, height = image.size
+            steps_x = int(width / accuracy)
+            steps_y = int(height / accuracy)
+
+            for step_x in range(steps_x):
+                for step_y in range(steps_y):
+                    node_id = str(uuid.uuid4())
+                    pos_x = int(step_x * accuracy + accuracy / 2)
+                    pos_y = int(step_y * accuracy + accuracy / 2)
+                    if image.getpixel((pos_x, pos_y)) == (0, 0, 0):
+                        cur.execute("INSERT INTO nodes VALUES (?,?,?)", [node_id, pos_x, pos_y])
+                        self.nodes[node_id] = [pos_x, pos_y]
+            db.commit()
+            print('Nodes created!')
+
+        def generate_edges():
+            def euklidian_distance(x1, y1, x2, y2):
+                return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+            cur = db.cursor()
+            for node_id in self.nodes.keys():
+                node_pos_x = self.nodes[node_id][0]
+                node_pos_y = self.nodes[node_id][1]
+                bound_x_lower = int(node_pos_x - (accuracy + math.sqrt(accuracy)))
+                bound_x_upper = int(math.ceil(node_pos_x + (accuracy + math.sqrt(accuracy))))
+                bound_y_lower = int(node_pos_y - (accuracy + math.sqrt(accuracy)))
+                bound_y_upper = int(math.ceil(node_pos_y + (accuracy + math.sqrt(accuracy))))
+                print(node_id, bound_x_lower, bound_x_upper, bound_y_lower, bound_y_upper)
+                edge_list = list()
+                for neighbor in cur.execute(
+                    "SELECT * from nodes "
+                    "WHERE pos_x BETWEEN ? AND ? "
+                    "AND pos_y BETWEEN ? and ?; ",
+                    (
+                        bound_x_lower,
+                        bound_x_upper,
+                        bound_y_lower,
+                        bound_y_upper
+                    )
+                ):
+                    neighbor_id = neighbor[0]
+                    if neighbor_id != node_id:
+                        edge_id = str(uuid.uuid4())
+                        neighbor_pos_x = self.nodes[neighbor_id][0]
+                        neighbor_pos_y = self.nodes[neighbor_id][1]
+                        distance = euklidian_distance(
+                            node_pos_x,
+                            node_pos_y,
+                            neighbor_pos_x,
+                            neighbor_pos_y)
+                        edge_list.append([edge_id, node_id, neighbor_id, distance])
+                for edge in edge_list:
+                    cur.execute(
+                        "INSERT INTO edges VALUES (?,?,?,?)",
+                        [edge[0], edge[1], edge[2], edge[3]]
+                    )
+                    self.edges[edge[0]] = {
+                        'connection': [
+                            edge[1],
+                            edge[2]
+                        ],
+                        'distance': edge[3]
+                    }
+            print('Edges created!')
+
+        generate_nodes()
+        generate_edges()
