264d498275429ff93dcbba4666a4cf4ceb69611e
SebastianProdaso <s.heger@prodaso.de>
2020-09-09 08:22:48 +0200

    Adds graph output

--- Graph/graphCreation.py
+++ Graph/graphCreation.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import math
 import uuid
 
-from PIL import Image
+from PIL import Image, ImageDraw
 
 
 def database_reset(db):
@@ -43,9 +43,10 @@ def database_reset(db):
 
 
 class Graph:
-    def __init__(self, db, reinit):
+    def __init__(self, db, reinit, graph_accuracy):
         self.nodes = dict()
         self.edges = dict()
+        self.graph_accuracy = graph_accuracy
 
         if reinit:
             database_reset(db)
@@ -57,6 +58,7 @@ class Graph:
                     item[1],
                     item[2]
                 ]
+            print('Nodes loaded!')
             for item in cur.execute('SELECT * FROM edges'):
                 self.edges[item[0]] = {
                     'connection': [
@@ -66,22 +68,23 @@ class Graph:
                     'distance': item[3]
                 }
             db.commit()
+            print('Edges loaded!')
 
-    def generate_graph(self, db, accuracy=5):
+    def generate_graph(self, db):
         def generate_nodes():
             cur = db.cursor()
             path = Path().parent / 'Input'
             filename = 'MAP.png'
             image = Image.open(path / filename, 'r')
             width, height = image.size
-            steps_x = int(width / accuracy)
-            steps_y = int(height / accuracy)
+            steps_x = int(width / self.graph_accuracy)
+            steps_y = int(height / self.graph_accuracy)
 
             for step_x in range(steps_x):
                 for step_y in range(steps_y):
                     node_id = str(uuid.uuid4())
-                    pos_x = int(step_x * accuracy + accuracy / 2)
-                    pos_y = int(step_y * accuracy + accuracy / 2)
+                    pos_x = int(step_x * self.graph_accuracy + self.graph_accuracy / 2)
+                    pos_y = int(step_y * self.graph_accuracy + self.graph_accuracy / 2)
                     if image.getpixel((pos_x, pos_y)) == (0, 0, 0):
                         cur.execute("INSERT INTO nodes VALUES (?,?,?)", [node_id, pos_x, pos_y])
                         self.nodes[node_id] = [pos_x, pos_y]
@@ -96,11 +99,10 @@ class Graph:
             for node_id in self.nodes.keys():
                 node_pos_x = self.nodes[node_id][0]
                 node_pos_y = self.nodes[node_id][1]
-                bound_x_lower = int(node_pos_x - (accuracy + math.sqrt(accuracy)))
-                bound_x_upper = int(math.ceil(node_pos_x + (accuracy + math.sqrt(accuracy))))
-                bound_y_lower = int(node_pos_y - (accuracy + math.sqrt(accuracy)))
-                bound_y_upper = int(math.ceil(node_pos_y + (accuracy + math.sqrt(accuracy))))
-                print(node_id, bound_x_lower, bound_x_upper, bound_y_lower, bound_y_upper)
+                bound_x_lower = int(node_pos_x - (self.graph_accuracy + math.sqrt(self.graph_accuracy)))
+                bound_x_upper = int(math.ceil(node_pos_x + (self.graph_accuracy + math.sqrt(self.graph_accuracy))))
+                bound_y_lower = int(node_pos_y - (self.graph_accuracy + math.sqrt(self.graph_accuracy)))
+                bound_y_upper = int(math.ceil(node_pos_y + (self.graph_accuracy + math.sqrt(self.graph_accuracy))))
                 edge_list = list()
                 for neighbor in cur.execute(
                     "SELECT * from nodes "
@@ -140,3 +142,26 @@ class Graph:
 
         generate_nodes()
         generate_edges()
+
+    def generate_graph_output(self, image_filepath_input, image_filepath_output, node_radius=1):
+        image = Image.open(image_filepath_input, 'r').convert('RGB')
+        draw = ImageDraw.Draw(image)
+        for edge in self.edges:
+            n1 = self.edges[edge]['connection'][0]
+            n2 = self.edges[edge]['connection'][1]
+            x1, y1 = self.nodes[n1]
+            x2, y2 = self.nodes[n2]
+            draw.line(
+                xy=(x1, y1, x2, y2),
+                fill=(0, 100, 0)
+            )
+
+        for node in self.nodes:
+            x1, y1 = self.nodes[node]
+            draw.ellipse(
+                xy=(x1 - node_radius, y1 - node_radius, x1 + node_radius, y1 + node_radius),
+                fill=(0, 255, 0)
+            )
+
+        image.save(image_filepath_output, 'PNG')
+        print('Graph output created!')
--- control.py
+++ control.py
@@ -6,4 +6,12 @@ from Graph import graphCreation
 if __name__ == '__main__':
     db_path = Path() / 'database.db'
     with sqlite3.connect(db_path) as db:
-        graph = graphCreation.Graph(db=db, reinit=True)
+        graph = graphCreation.Graph(
+            db=db,
+            reinit=True,
+            graph_accuracy=10
+        )
+        graph.generate_graph_output(
+            image_filepath_input=Path() / 'Input' / 'MAP.png',
+            image_filepath_output=Path() / 'Output' / 'GRAPH.png'
+        )
