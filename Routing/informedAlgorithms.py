dfbe51147201ffef595ba5d88c960bb74c4b52c7
SebastianProdaso <s.heger@prodaso.de>
2020-09-09 12:19:41 +0200

    Adds framing for route planning algos Dijkstra and Astar

---
+++ Routing/informedAlgorithms.py
@@ -0,0 +1,8 @@
+import random
+
+
+class AStar:
+    def __init__(self, graph):
+        node_start = random.choice(list(graph.nodes.keys()))
+        node_target = random.choice(list(graph.nodes.keys()))
+        print(f'Route planning from {node_start} to {node_target} with Dijkstra-Algorithm.')
--- control.py
+++ control.py
@@ -2,16 +2,20 @@ from pathlib import Path
 import sqlite3
 
 from Graph import graphCreation
+from Routing import uninformedAlgorithms, informedAlgorithms
+
 
 if __name__ == '__main__':
     db_path = Path() / 'database.db'
     with sqlite3.connect(db_path) as db:
         graph = graphCreation.Graph(
             db=db,
-            reinit=True,
+            reinit=False,
             graph_accuracy=10
         )
         graph.generate_graph_output(
             image_filepath_input=Path() / 'Input' / 'MAP.png',
             image_filepath_output=Path() / 'Output' / 'GRAPH.png'
         )
+        uninformedAlgorithms.Dijksta(graph=graph)
+        informedAlgorithms.AStar(graph=graph)
